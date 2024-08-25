48f6ca0bfd9b002d63c909c7c21957e1aa5894f8
Dreizack97 <dreizack97@gmail.com>
2024-08-25 14:49:45 -0600

    Condiciones con operadores

--- Condiciones.py
+++ Condiciones.py
@@ -1,8 +1,12 @@
 print("Introduce la temperatura actual:")
 temperatura = float(input())
 
-if (temperatura >= 27):
-    print("Compra un helado.")
-    print("Test")
+print("¿Cuanto dinero tienes?")
+dinero = int(input())
 
+if (temperatura >= 27 and dinero >= 5):
+    print("Compra un helado.")
+    dinero -= 5
+
+print("Tu cambio es: " + str(dinero))
 print("Adios")
